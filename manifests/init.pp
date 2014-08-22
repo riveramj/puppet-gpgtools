bdf3f28c3160468fbee043c9ae2cc48eb3e767ba
Mike Rivera <rivera.mj@gmail.com>
2014-08-21 23:41:03 -0400

    try http download

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,6 @@ class gpgtools {
   package { 'GPGTools':
     name     => 'Install.pkg',
     provider => 'pkgdmg',
-    source   => 'https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg',
+    source   => 'http://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg',
   }
 }
