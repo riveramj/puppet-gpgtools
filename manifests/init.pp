52ddf2cf477e6b60c0d31da684b10e9247a034ce
Mike Rivera <rivera.mj@gmail.com>
2014-08-21 23:36:48 -0400

    update to latest version

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,6 @@ class gpgtools {
   package { 'GPGTools':
     name     => 'Install.pkg',
     provider => 'pkgdmg',
-    source   => 'https://www.dropbox.com/s/udovo74flam4vnp/GPG%20Suite%20-%202013.10.22.dmg',
+    source   => 'https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg',
   }
 }
